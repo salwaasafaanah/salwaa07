b17f5e4a1cb2be5ed94cf9121644fedd885c57f4
salwaasafaanah <salwaasafaanah07@gmail.com>
2026-05-22 14:16:48 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,8 +6,6 @@ st.write(
 )
 import streamlit as st
 
-st.latex(r'''
-    a + ar + a r^2 + a r^3 + \cdots + a r^{n-1} =
-    \sum_{k=0}^{n-1} ar^k =
-    a \left(\frac{1-r^{n}}{1-r}\right)
-    ''')
+st.text("This is text\n[and more text](that's not a Markdown link).")
+
+Copy
