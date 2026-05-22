4d83c019b8f842610d316d985e14cd556da35f25
salwaasafaanah <salwaasafaanah07@gmail.com>
2026-05-22 14:18:57 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,13 @@ st.title("🎈 Aplikasi Awaa")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+import pandas as pd
 import streamlit as st
+from numpy.random import default_rng as rng
 
-st.text("This is text\n[and more text](that's not a Markdown link).")
+df = pd.DataFrame(
+    rng(0).standard_normal((1000, 2)) / [50, 50] + [37.76, -122.4],
+    columns=["lat", "lon"],
+)
 
-
+st.map(df)
