a03eb653042c7da66a9f3190f0eb0bb03033e516
salwaasafaanah <salwaasafaanah07@gmail.com>
2026-05-22 14:20:30 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,13 +4,9 @@ st.title("🎈 Aplikasi Awaa")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
-import pandas as pd
 import streamlit as st
-from numpy.random import default_rng as rng
 
-df = pd.DataFrame(
-    rng(0).standard_normal((1000, 2)) / [50, 50] + [37.76, -122.4],
-    columns=["lat", "lon"],
-)
-
-st.map(df)
+st.page_link("your_app.py", label="Home", icon="🏠")
+st.page_link("pages/page_1.py", label="Page 1", icon="1️⃣")
+st.page_link("pages/page_2.py", label="Page 2", icon="2️⃣", disabled=True)
+st.page_link("http://www.google.com", label="Google", icon="🌎")
